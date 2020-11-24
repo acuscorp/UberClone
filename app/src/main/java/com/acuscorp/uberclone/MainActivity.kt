b101e2c916a7af0c52468a3d99c0a8b48b6c7a6e
acuscorp <acuscorp@gmail.com>
2020-11-23 20:48:21 -0600

    login implementation with firebase

--- app/src/main/java/com/acuscorp/uberclone/MainActivity.kt
+++ app/src/main/java/com/acuscorp/uberclone/MainActivity.kt
@@ -2,6 +2,7 @@ package com.acuscorp.uberclone
 
 import android.content.Context
 import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.LayoutInflater
@@ -55,9 +56,62 @@ class MainActivity : AppCompatActivity() {
         rootLayout = findViewById(R.id.rootLayout)
 
         // event listener
+        btnSignIn.setOnClickListener {
+            showLoginDialog()
+        }
+
         btnRegister.setOnClickListener {
             showRegisterDialog()
         }
+    }
+
+    private fun showLoginDialog() {
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("SIGN IN")
+            .setMessage("Please use email to sign in")
+
+        val inflater = LayoutInflater.from(this)
+        val loginLayout = inflater.inflate(R.layout.layout_login, null)
+
+        val edtEmail: MaterialEditText = loginLayout.findViewById(R.id.edtEmail)
+        val edtPassword: MaterialEditText = loginLayout.findViewById(R.id.edtPassword)
+
+
+        dialog.setView(loginLayout)
+
+        dialog.setPositiveButton("SIGN IN") { dialog, which ->
+            dialog.dismiss()
+
+            // check validation
+            if (TextUtils.isEmpty(edtEmail.text)) {
+                Snackbar.make(rootLayout, "Please enter email address", Snackbar.LENGTH_SHORT)
+                    .show()
+                return@setPositiveButton
+            }
+
+            if (edtPassword.text.toString().length < 6) {
+                Snackbar.make(rootLayout, "Password too short", Snackbar.LENGTH_SHORT).show()
+                return@setPositiveButton
+            }
+
+            //register new user
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password )
+                .addOnSuccessListener {
+                    startActivity(Intent(this,Welcome::class.java))
+                    finish()
+                }
+                .addOnFailureListener {
+                    Snackbar.make(rootLayout,"Failde $it",Snackbar.LENGTH_SHORT).show()
+                }
+        }
+        dialog.setNegativeButton("CANCEL") { dialog, which ->
+            dialog.dismiss()
+        }
+        dialog.show()
+
     }
 
     private fun showRegisterDialog() {
@@ -72,6 +126,10 @@ class MainActivity : AppCompatActivity() {
         val edtPassword: MaterialEditText = registerLayout.findViewById(R.id.edtPassword)
         val edtName: MaterialEditText = registerLayout.findViewById(R.id.edtName)
         val edtPhone: MaterialEditText = registerLayout.findViewById(R.id.edtPhone)
+
+
+
+        dialog.setView(registerLayout)
 
         dialog.setPositiveButton("REGISTER") { dialog, which ->
             dialog.dismiss()
@@ -96,8 +154,8 @@ class MainActivity : AppCompatActivity() {
             //register new user
             val email = edtEmail.text.toString()
             val password = edtPassword.text.toString()
-            val phone = edtPhone.text.toString()
             val name = edtName.text.toString()
+            val phone = edtPhone.text.toString()
             auth.createUserWithEmailAndPassword(email, password)
                 .addOnSuccessListener {
                     // save user to db
@@ -107,12 +165,29 @@ class MainActivity : AppCompatActivity() {
                         password = password,
                         phone = phone
                     )
+                    users.child(FirebaseAuth.getInstance().currentUser!!.uid)
+                        .setValue(user)
+                        .addOnSuccessListener {
+                            Snackbar.make(
+                                rootLayout,
+                                "Register successfully",
+                                Snackbar.LENGTH_SHORT
+                            ).show()
+                        }
+                        .addOnFailureListener {
+                            Snackbar.make(rootLayout, "Failed $it", Snackbar.LENGTH_SHORT).show()
+                        }
 
+
+                }
+                .addOnFailureListener {
+                    Snackbar.make(rootLayout, "Failed $it", Snackbar.LENGTH_SHORT).show()
                 }
 
         }
         dialog.setNegativeButton("CANCEL") { dialog, which ->
             dialog.dismiss()
         }
+        dialog.show()
     }
 }
